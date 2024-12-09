6313a376401c8957a0cc4e0e55659b9210858c23
jet_penguin <m1910625@gl.cc.uec.ac.jp>
2024-12-10 00:16:54 +0900

    add solve pokemon crossword with  no solution

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,3 +27,23 @@ def test_solve_pokemon_crossword_basic():
         ["#", "ゴ", "#", "#", "#"],
         ["#", "ン", "#", "#", "#"],
     ]
+
+def test_solve_pokemon_crossword_no_solution():
+    rows, cols = 4, 5
+    binaryGrid = [
+        [1, 1, 1, 1, 1],
+        [0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0],
+    ]
+    charGrid = [
+        ["ン", "", "", "ュ", "ウ"],
+        ["",  "", "", "", ""],
+        ["", "ゴ", "", "", ""],
+        ["", "ン", "", "", ""],
+    ]
+    # テスト用JSONファイル
+    pokemon_names_file = "tests/data/pokemon_names.json"
+    
+    result = solve_pokecrossword(rows, cols, binaryGrid, charGrid, pokemon_names_file)
+    assert result is None
